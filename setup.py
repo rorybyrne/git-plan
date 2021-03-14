c3ddc4a8f399dc0cddc22bec20f0bfba128ab518
Luke Stedman <luke@stedman.tech>
2021-03-14 12:13:53 +0000

    _version file no longer included in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     use_scm_version=True,
     packages=find_packages(exclude=['tests']),
     setup_requires=['setuptools_scm'],
-    package_data={'': ['git_plan/_version']},
     include_package_data=True,
     install_requires=[
         'dependency_injector[yaml]',
