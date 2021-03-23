81291b0073cadada42ba62f5dba214117a7e67ed
synek <rory@rory.bio>
2021-03-23 12:09:49 +0000

    GitService has basic tests for failure modes

--- git_plan/service/git.py
+++ git_plan/service/git.py
@@ -3,6 +3,7 @@
 Author: Rory Byrne <rory@rory.bio>
 """
 import subprocess
+from typing import List, Optional
 
 from git_plan.exceptions import CommitAbandoned, GitException
 from git_plan.model.commit import Commit
@@ -11,42 +12,50 @@ from git_plan.model.commit import Commit
 class GitService:
     """Interface to git functionality"""
 
+    COMMIT = 'git commit -e -m'
+    HAS_STAGED = 'git diff --staged --quiet'
+    GET_BRANCH = 'git branch --show-current'
+
     def __init__(self):
         pass
 
-    @staticmethod
-    def commit(commit: Commit):
+    def commit(self, commit: Commit):
         """Runs git commit with the given commit-plan as a template"""
-        cmd = 'git commit -e -m'.split(' ')
+        cmd = self.COMMIT.split(' ')
         cmd.append(str(commit.message))
 
         try:
-            subprocess.run(cmd, check=True)
+            self._run_command(cmd, capture_output=False)
         except subprocess.CalledProcessError as e:
             raise CommitAbandoned() from e
 
-    @staticmethod
-    def has_staged_files() -> bool:
+    def has_staged_files(self) -> bool:
         """Returns True if there are staged files, and False otherwise"""
-        cmd = 'git diff --staged --quiet'.split(' ')
+        cmd = self.HAS_STAGED.split(' ')
 
         try:
-            subprocess.run(cmd, check=True)
+            self._run_command(cmd)
             return False
         except subprocess.CalledProcessError:
             return True
 
-    @staticmethod
-    def get_current_branch():
+    def get_current_branch(self):
         """Gets the current branch via git"""
-        cmd = 'git branch --show-current'.split(' ')
+        cmd = self.GET_BRANCH.split(' ')
 
         try:
-            result = subprocess.run(cmd, capture_output=True, check=True)
-            branch = result.stdout.decode('utf-8')
+            branch = self._run_command(cmd)
             if not branch or branch == '':
                 raise GitException(f'Invalid branch: "{branch}"')
 
             return branch.strip()
         except subprocess.CalledProcessError as e:
             raise GitException('Failed to get current git branch') from e
+
+    @staticmethod
+    def _run_command(cmd: List[str], capture_output: bool = True) -> Optional[str]:
+        result = subprocess.run(cmd, capture_output=capture_output, check=True)
+        if result.stdout:
+            return result.stdout.decode()
+
+        return None
