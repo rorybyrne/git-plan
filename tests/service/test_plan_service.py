42c6fbef41f860c263b3b1280eb430f24642c341
Rory Byrne <rory@rory.bio>
2021-03-27 10:08:26 +0000

    Improves parsing of the default subcommand (#74)
    What does this commit do?
    =========================
    * Chooses a default subcommand (list/add) in cli.py, instead of via
    a default "plan.py" subcommand
    * Deletes "plan.py"
    * Adds `-a` flag to list plans on all branches
    * Adds comments to "cli.py"
    * Removes the help command
    
    Why are we doing this?
    ======================
    * To simplify the architecture of the CLI
    * Adds a test
    
    Notes
    =====
    Closes #51
    Closes #65

--- tests/service/test_plan_service.py
+++ tests/service/test_plan_service.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 import pytest
 
 from git_plan.exceptions import ProjectNotInitialized
-from git_plan.model.commit import CommitMessage
+from git_plan.model.commit import Commit, CommitMessage
 from git_plan.model.project import Project
 from git_plan.service.git import GitService
 from git_plan.service.plan import PlanService
@@ -37,7 +37,6 @@ def test_add_commit_should_raise_not_initialized(mock_get_current_branch, mock_p
 @patch('git_plan.service.plan.PlanService._prompt_user_for_plan')
 @patch.object(GitService, 'get_current_branch')
 def test_should_create_commit(mock_get_current_branch, mock_prompt_user):
-
     mock_prompt_user.return_value = CommitMessage('headline', 'body')
     mock_get_current_branch.return_value = 'foo_branch'
 
@@ -53,3 +52,20 @@ def test_should_create_commit(mock_get_current_branch, mock_prompt_user):
         assert commit.message.headline == 'headline'
         assert Path(commit.path).is_file()
 
+@patch('git_plan.service.plan.PlanService._prompt_user_for_plan')
+@patch.object(GitService, 'get_current_branch')
+def test_deleting_nonexistent_plan_raises_exception(mock_get_current_branch, mock_prompt_user):
+    mock_prompt_user.return_value = CommitMessage('headline', 'body')
+    mock_get_current_branch.return_value = 'foo_branch'
+
+    git_service = GitService()
+    plan_service = PlanService('.', '.', git_service)
+    with tempfile.TemporaryDirectory() as tempdir:
+        Path(tempdir, '.git').mkdir()
+        Path(tempdir, '.plan').mkdir()
+        Path(tempdir, '.plan', 'plans').mkdir()
+        project = Project(tempdir)
+
+        commit = plan_service._create_commit(project, 'some_id')
+        with pytest.raises(RuntimeError):
+            plan_service.delete_commit(commit)
